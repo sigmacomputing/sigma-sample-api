e7a48178f40245d5f82064fc8e00992085a15162
Brett Bedevian <brett@sigmacomputing.com>
2024-10-04 09:41:12 -0400

    reformat with formatter and oliver tab suggestion

--- bulk_reassign_docs.py
+++ bulk_reassign_docs.py
@@ -21,20 +21,25 @@ def get_member_id(client, user_email):
     # HTTP and other errors are handled generally by raising them as exceptions to be surfaced in downstream code
     except requests.exceptions.HTTPError as errh:
         # The API response's message value is sent in lieu of the full response to display to the user for clarity
-        raise Exception(errh.response.status_code, f"API message: {errh.response.json()['message']}")
+        raise Exception(errh.response.status_code,
+                        f"API message: {errh.response.json()['message']}")
     except requests.exceptions.ConnectionError as errc:
-        raise Exception(f"Connection Error: {errc}, API response: {errc.response.text}")
+        raise Exception(
+            f"Connection Error: {errc}, API response: {errc.response.text}")
     except requests.exceptions.Timeout as errt:
-        raise Exception(f"Timeout Error: {errt}, API response: {errt.response.text}")
+        raise Exception(
+            f"Timeout Error: {errt}, API response: {errt.response.text}")
     except requests.exceptions.RequestException as err:
-        raise Exception(f"Other Error: {err}, API response: {err.response.text}")
+        raise Exception(
+            f"Other Error: {err}, API response: {err.response.text}")
     else:
         data = response.json()
         if len(data['entries']) == 0:
             print("No users found with this email:", user_email)
-            raise SystemExit("Script aborted")   
+            raise SystemExit("Script aborted")
         else:
             return data['entries'][0]['memberId']
+
 
 def get_member_files(client, user_id):
     """ Update member
@@ -54,13 +59,17 @@ def get_member_files(client, user_id):
             response.raise_for_status()
 
         except requests.exceptions.HTTPError as errh:
-            raise Exception(f"Connection Error: {errh}, API response: {errh.response.text}")
+            raise Exception(
+                f"Connection Error: {errh}, API response: {errh.response.text}")
         except requests.exceptions.ConnectionError as errc:
-            raise Exception(f"Connection Error: {errc}, API response: {errc.response.text}")
+            raise Exception(
+                f"Connection Error: {errc}, API response: {errc.response.text}")
         except requests.exceptions.Timeout as errt:
-            raise Exception(f"Timeout Error: {errt}, API response: {errt.response.text}")
+            raise Exception(
+                f"Timeout Error: {errt}, API response: {errt.response.text}")
         except requests.exceptions.RequestException as err:
-            raise Exception(f"Other Error: {err}, API response: {err.response.text}")
+            raise Exception(
+                f"Other Error: {err}, API response: {err.response.text}")
         else:
             resp = response.json()
             data = data + resp['entries']
@@ -70,6 +79,7 @@ def get_member_files(client, user_id):
                 pageID = str(resp['nextPage'])
                 nextPage = f'&page={pageID}'
     return data
+
 
 def update_file(client, user_id, file_id):
     """ Update file
@@ -84,24 +94,29 @@ def update_file(client, user_id, file_id):
     try:
         response = client.patch(
             f"v2/files/{file_id}",
-            json={ "ownerId":user_id }
+            json={"ownerId": user_id}
         )
         response.raise_for_status()
     # HTTP and other errors are handled generally by raising them as exceptions to be surfaced in downstream code
     except requests.exceptions.HTTPError as errh:
         # The API response's message value is sent in lieu of the full response to display to the user for clarity
         # Certain error response messages are useful for the user troubleshoot common issues, such as invalid Member Type or New Email already in use
-        raise Exception(errh.response.status_code, f"API message: {errh.response.json()['message']}")
+        raise Exception(errh.response.status_code,
+                        f"API message: {errh.response.json()['message']}")
     except requests.exceptions.ConnectionError as errc:
-        raise Exception(f"Connection Error: {errc}, API response: {errc.response.text}")
+        raise Exception(
+            f"Connection Error: {errc}, API response: {errc.response.text}")
     except requests.exceptions.Timeout as errt:
-        raise Exception(f"Timeout Error: {errt}, API response: {errt.response.text}")
+        raise Exception(
+            f"Timeout Error: {errt}, API response: {errt.response.text}")
     except requests.exceptions.RequestException as err:
-        raise Exception(f"Other Error: {err}, API response: {err.response.text}")
+        raise Exception(
+            f"Other Error: {err}, API response: {err.response.text}")
     else:
         data = response.json()
-        print("transfer of document:",file_id, "---", response )
+        print("transfer of document:", file_id, "---", response)
         return data
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -120,25 +135,29 @@ def main():
         '--new_owner', type=str, required=True, help='Org Member who you want to transfer the documents to')
 
     args = parser.parse_args()
-    client = SigmaClient(args.env, args.cloud, args.client_id, args.client_secret)
-    
+    client = SigmaClient(args.env, args.cloud,
+                         args.client_id, args.client_secret)
+
     # we need to confirm that both the existing user and new user are valid "check_users" fn
     try:
         curr_owner_id = get_member_id(client, args.curr_owner)
     except Exception as e:
         print(f"{e}")
         raise SystemExit("Script aborted")
+
     try:
         new_owner_id = get_member_id(client, args.new_owner)
     except Exception as e:
         print(f"{e}")
         raise SystemExit("Script aborted")
+
     # get files of curr_user
     try:
         member_files = get_member_files(client, curr_owner_id)
     except Exception as e:
         print(f"{e}")
         raise SystemExit("Script aborted")
+
     # filter to only docs they own
     filtered_arr = [p for p in member_files if p['ownerId'] == curr_owner_id]
     # loop through and reassign ownership
@@ -148,7 +167,6 @@ def main():
         except Exception as e:
             print(f"{e}")
             raise SystemExit("Script aborted")
-
 
 
 if __name__ == '__main__':
