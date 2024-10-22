c74b7f682f1f8d4406478a660d04c4ad542ee9cf
Brett Bedevian <brett@sigmacomputing.com>
2024-10-22 10:50:51 -0400

    updates cont

--- bulk_reassign_docs.py
+++ bulk_reassign_docs.py
@@ -53,12 +53,12 @@ def get_member_files(client, user_id):
     """
     data = []
     moreResults = True
-    nextPage = ''
+    next_page = ''
     while moreResults:
         try:
             # currently looks for workbooks and datasets but can be changed as needed
             response = client.get(
-                f"v2/members/{user_id}/files?typeFilters=workbook&typeFilters=dataset&limit=1000{nextPage}"
+                f"v2/members/{user_id}/files?typeFilters=workbook&typeFilters=dataset&limit=1000{next_page}"
             )
             response.raise_for_status()
 
@@ -80,24 +80,49 @@ def get_member_files(client, user_id):
             if resp['nextPage'] is None:
                 moreResults = False
             else:
-                pageID = str(resp['nextPage'])
-                nextPage = f'&page={pageID}'
+                page_id = str(resp['nextPage'])
+                next_page = f'&page={page_id}'
     return data
 
 
-def update_file(client, user_id, file_id, folderID):
+def update_file(client, user_id, file_id, folder_id):
     """ Update file
 
         :access_token:  Generated access token
-        :userId:        ID of the new owner
+        :user_id:        ID of the new owner
         :file_id:       File to transfer ownership of
 
         :returns:       Response JSON
 
     """
     updateFileBody={"ownerId": user_id}
-    if folderID:
-        updateFileBody["parentId"] = folderID
+    if folder_id:
+        # we need to make sure this person owns the folder
+        try:
+            response = client.get(
+            f"v2/files/{folder_id}",
+            )
+            response.raise_for_status()
+        # HTTP and other errors are handled generally by raising them as exceptions to be surfaced in downstream code
+        except requests.exceptions.HTTPError as errh:
+            raise Exception(errh.response.status_code,
+                            f"API message: {errh.response.json()['message']}")
+        except requests.exceptions.ConnectionError as errc:
+            raise Exception(
+                f"Connection Error: {errc}, API response: {errc.response.text}")
+        except requests.exceptions.Timeout as errt:
+            raise Exception(
+                f"Timeout Error: {errt}, API response: {errt.response.text}")
+        except requests.exceptions.RequestException as err:
+            raise Exception(
+                f"Other Error: {err}, API response: {err.response.text}")
+        else:
+            get_folder_response = response.json()
+            if get_folder_response['type'] == "folder":
+                updateFileBody["parentId"] = folder_id
+            else:
+                print("Provided folder_id not found")
+                raise SystemExit("Script aborted")
     try:
         response = client.patch(
             f"v2/files/{file_id}",
@@ -170,9 +195,9 @@ def main():
     # filter to only docs they own
     filtered_arr = [file for file in member_files if file['ownerId'] == curr_owner_id]
     # loop through and reassign ownership
-    for ownedDoc in filtered_arr:
+    for owned_doc in filtered_arr:
         try:
-            update_member_response = update_file(client, new_owner_id, ownedDoc['id'], args.new_folder)
+            update_file(client, new_owner_id, owned_doc['id'], args.new_folder)
         except Exception as e:
             print(f"{e}")
             raise SystemExit("Script aborted")
